3cc24d2fcc614a2b13d758a67149d83fac01edd7
FlorianZunke <florianzunke@gmx.de>
2025-11-30 13:54:04 +0100

    add Own Usermodel and update settings

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=255, blank=True, null=True)
+    activation_uid = models.CharField(max_length=255, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    
--- core/settings.py
+++ core/settings.py
@@ -174,3 +174,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "auth_app.CustomUser"
