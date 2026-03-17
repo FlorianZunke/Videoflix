ac76f42d2db717351b711412e3d0f06055f79be8
Florian Zunke <florianzunke@gmx.de>
2026-03-17 10:37:39 +0100

    update video_model

--- video_app/models.py
+++ video_app/models.py
@@ -7,7 +7,7 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     video_file = models.FileField(upload_to='videos/')
     thumbnail_url = models.FileField(upload_to='thumbnails/', null=True, blank=True)
-    category = models.CharField(max_length=100, null=True, blank=True)
+    category = models.CharField(max_length=100)
     conversion_status = models.CharField(
         max_length=20, 
         default='pending', 
